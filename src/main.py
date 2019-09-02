7cc46df77e00938b4b9e3987111fdbeca61a398c
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-02 11:52:42 -0400

    added argparse

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as  plt
+import argparse
 
 def reg_linear(x_linha,y_linha,alpha,no_it):
 	m=len(x_linha)
@@ -21,20 +22,26 @@ def reg_linear(x_linha,y_linha,alpha,no_it):
 		thetao=thetao-((alpha/m)*(X.T.dot(e)))
 	return errs,thetao.reshape((2))
 
+parser=argparse.ArgumentParser()
+parser.add_argument("--no_it","-n",type=int)
+parser.add_argument("--interval","-i",type=str)
+args=parser.parse_args()
+
+NO_IT=args.no_it
+a,b,c=[float(o) for o in args.interval.split(",")]
+
 x_linha=np.genfromtxt("../samples/entradas_x.txt")
 y_linha=np.genfromtxt("../samples/saidas_y.txt")
 
 m=len(x_linha)
 plt.rc('text', usetex=True)
-NO_IT=1000
 xs=np.array(range(1,NO_IT+1))
-for i in np.arange(2,5.1,.5):
+for i in np.arange(a,b+c,c):
 	prec=10**(-i)
 	errs,thetao=reg_linear(x_linha,y_linha,prec,NO_IT)
 	plt.plot(xs,errs,label=rf"$\alpha$=$10^{{{-i}}}$ $\theta={list(thetao)}$")
 plt.legend(fontsize=8)
 plt.xlabel('Números de iteração')
 plt.ylabel(r'$J(\theta)$')
-plt.title(r'Gráfico de $J(\theta)$ para diferente alphas')
+plt.title(rf'Gráfico de $J(\theta)$ para diferentes alphas que variam $10^{{{-a}}}$ e $10^{{{-b}}}$')
 plt.show()
-# print(errs)
