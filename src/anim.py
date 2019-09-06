513a0bd230d686a56f85b1ef652d7a739f3bbf94
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-06 09:08:54 -0400

    trying for perf in anim

--- src/anim.py
+++ src/anim.py
@@ -17,21 +17,23 @@ class Animation:
 		self.X=np.ones((self.m,2))
 		self.X[:,1]=self.x_linha
 		self.thetao=np.array([[0],[0]])
-		self.axis=[
-			min(self.x_linha)-.1, 
-			max(self.x_linha)+.1,
-			min(self.y_linha)-.1, 
-			max(self.y_linha)+.1
-		]
+		border=.1
+		self.axis=np.array([
+			min(self.x_linha)-border, 
+			max(self.x_linha)+border,
+			min(self.y_linha)-border, 
+			max(self.y_linha)+border
+		])
 
 	def animate(self,i):
 		ax1.clear()
 		plt.axis(self.axis)
 		ax1.scatter(self.x_linha, self.y_linha)
 		t0,t1=self.thetao.reshape(2)
+		h=lambda xs:[t0+(t1*x) for x in xs]
 		ax1.plot(
 			self.axis[:2],
-			[self.thetao.reshape(2).dot(np.array([1,x])) for x in self.axis[:2]],
+			h(self.axis[:2]),
 			color="red",
 			label=rf"$\theta={[t0,t1]}$"
 		)
