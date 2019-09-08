6b90b0eefa4ca1399ab58b5eacdd74b36418d329
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-08 16:15:13 -0400

    added module that calculates n inputs and normalize

--- src/anim.py
+++ src/anim.py
@@ -4,19 +4,14 @@ import matplotlib.animation as animation
 import numpy as np
 import time
 import argparse
+from LinearRegression import LinearRegression
 
 class Animation:
-	def __init__(self,alpha,skip=1):
+	def __init__(self,alpha,skip=1,normalize):
 		self.skip=skip
-		self.alpha=alpha
 		self.x_linha=np.genfromtxt("../samples/entradas_x.txt")
 		self.y_linha=np.genfromtxt("../samples/saidas_y.txt")
-
-		self.m=len(self.x_linha)
-		self.Y=self.y_linha.reshape(self.m,1)
-		self.X=np.ones((self.m,2))
-		self.X[:,1]=self.x_linha
-		self.thetao=np.array([[0],[0]])
+		self.model=LinearRegression(self.x_linha,self.y_linha,alpha=alpha,normalize=normalize)
 		border=.1
 		self.axis=np.array([
 			min(self.x_linha)-border, 
@@ -29,7 +24,7 @@ class Animation:
 		ax1.clear()
 		plt.axis(self.axis)
 		ax1.scatter(self.x_linha, self.y_linha)
-		t0,t1=self.thetao.reshape(2)
+		t0,t1=self.model.theta.reshape(2)
 		h=lambda xs:[t0+(t1*x) for x in xs]
 		ax1.plot(
 			self.axis[:2],
@@ -37,25 +32,22 @@ class Animation:
 			color="red",
 			label=rf"$\theta={[t0,t1]}$"
 		)
-		
 		for _ in range(self.skip):
-			H_theta=self.X.dot(self.thetao)
-			e=H_theta-self.Y
-			j=float(e.T.dot(e))/(2*self.m)
-			self.thetao=self.thetao-((self.alpha/self.m)*(self.X.T.dot(e)))
+			j=next(self.model)[1]
 
 		plt.xlabel('X')
 		plt.ylabel('Y')
-		plt.title(rf'$\alpha={self.alpha:.2e},it={i*self.skip},j={j:e}$') 
+		plt.title(rf'$\alpha={self.model.alpha:.2e},it={i*self.skip},j={j:e}$') 
 		plt.legend(fontsize=8)
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--alpha","-a",type=float,required=True)
 parser.add_argument("--skip","-s",type=int,required=True)
+parser.add_argument("--normalize",action="store_true")
 args=parser.parse_args()
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
-anim=Animation(args.alpha,args.skip)
+anim=Animation(args.alpha,args.skip,args.normalize)
 ani = animation.FuncAnimation(fig, anim.animate, interval=1) 
 plt.show()
--- src/main.py
+++ src/main.py
@@ -1,42 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as  plt
 import argparse
-
-def reg_linear(x_linha,y_linha,alpha,no_it):
-	m=len(x_linha)
-	Y=y_linha.reshape(m,1)
-	X=np.ones((m,2))
-	X[:,1]=x_linha
-	# t1*x+t0
-	thetao=np.array([
-		[0],
-		[0]
-	])
-	errs=np.zeros((no_it))
-	for i in range(no_it):
-		H_theta=X.dot(thetao)
-		e=H_theta-Y
-		j=(e.T.dot(e)/(2*m)).reshape(())
-		errs[i]=j
-		thetao=thetao-((alpha/m)*(X.T.dot(e)))
-	return errs,thetao.reshape((2))
+from LinearRegression import LinearRegression
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--no_it","-n",type=int,required=True)
 parser.add_argument("--interval","-i",type=str,required=True)
+parser.add_argument("--normalize",action="store_true")
 args=parser.parse_args()
 
-NO_IT=args.no_it
 a,b,c=[float(o) for o in args.interval.split(",")]
 
 x_linha=np.genfromtxt("../samples/entradas_x.txt")
 y_linha=np.genfromtxt("../samples/saidas_y.txt")
 
-m=len(x_linha)
 plt.rc('text', usetex=True)
-xs=np.array(range(1,NO_IT+1))
+xs=np.array(range(1,args.no_it+1))
 for alpha in np.arange(a,b+c,c):
-	errs,thetao=reg_linear(x_linha,y_linha,alpha,NO_IT)
+	model=LinearRegression(x_linha,y_linha,alpha=alpha,normalize=args.normalize)
+	errs=np.zeros((args.no_it))
+	for i in range(args.no_it):
+		thetao,j=next(model)
+		errs[i]=j
 	plt.plot(xs,errs,label=rf"$\alpha={alpha}$ $\theta={list(thetao)}$")
 plt.legend(fontsize=8)
 plt.xlabel('Números de iteração')
