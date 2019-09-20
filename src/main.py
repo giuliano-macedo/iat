77a5152c2b44b8f13e52569cf74a308b86564518
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-19 21:45:52 -0400

    better argparse

--- src/main.py
+++ src/main.py
@@ -2,21 +2,33 @@ import numpy as np
 import matplotlib.pyplot as  plt
 import argparse
 from LinearRegression import LinearRegression
+from ast import literal_eval
 
+def interval(s):
+	global GRAPH_TITLE
+	ans=literal_eval(s)
+	print(ans)
+	if isinstance(ans,tuple) and len(ans)==3 and all(isinstance(x, (float,int)) for x in ans):
+		a,b,c=ans
+		GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para diferentes alphas que variam ${a}$ e ${b}$, passo {c}'
+		return np.arange(a,b+c,c)
+	if isinstance(ans,list) and all(isinstance(x, (float,int)) for x in ans):
+		GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para os alphas {ans}'
+		return ans
+	raise TypeError()
+GRAPH_TITLE=""
 parser=argparse.ArgumentParser()
 parser.add_argument("--no_it","-n",type=int,required=True)
-parser.add_argument("--interval","-i",type=str,required=True)
+parser.add_argument("--interval","-i",type=interval,required=True)
 parser.add_argument("--normalize",action="store_true")
 args=parser.parse_args()
-
-a,b,c=[float(o) for o in args.interval.split(",")]
 
 x_linha=np.genfromtxt("../samples/entradas_x.txt")
 y_linha=np.genfromtxt("../samples/saidas_y.txt")
 
 plt.rc('text', usetex=True)
 xs=np.array(range(1,args.no_it+1))
-for alpha in np.arange(a,b+c,c):
+for alpha in args.interval:
 	model=LinearRegression(x_linha,y_linha,alpha=alpha,normalize=args.normalize)
 	errs=np.zeros((args.no_it))
 	for i in range(args.no_it):
@@ -26,5 +38,5 @@ for alpha in np.arange(a,b+c,c):
 plt.legend(fontsize=8)
 plt.xlabel('Números de iteração')
 plt.ylabel(r'$J(\theta)$')
-plt.title(rf'Gráfico de $J(\theta)$ para diferentes alphas que variam ${a}$ e ${b}$, passo {c}')
+plt.title(GRAPH_TITLE)
 plt.show()
