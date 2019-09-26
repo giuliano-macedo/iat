d53d217ff755cfd10ce6a02d5f1c671986e2ae0f
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-26 13:30:29 -0400

    modularized more with common

--- iat1/src/main.py
+++ iat1/src/main.py
@@ -5,26 +5,10 @@ import numpy as np
 import matplotlib.pyplot as  plt
 import argparse
 from ia import LinearRegression
+from ia.utils import common_argparse_alphas
 from ast import literal_eval
 
-def interval(s):
-	global GRAPH_TITLE
-	ans=literal_eval(s)
-	print(ans)
-	if isinstance(ans,tuple) and len(ans)==3 and all(isinstance(x, (float,int)) for x in ans):
-		a,b,c=ans
-		GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para diferentes alphas que variam ${a}$ e ${b}$, passo {c}'
-		return np.arange(a,b+c,c)
-	if isinstance(ans,list) and all(isinstance(x, (float,int)) for x in ans):
-		GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para os alphas {ans}'
-		return ans
-	raise TypeError()
-GRAPH_TITLE=""
-parser=argparse.ArgumentParser()
-parser.add_argument("--no_it","-n",type=int,required=True)
-parser.add_argument("--interval","-i",type=interval,required=True)
-parser.add_argument("--normalize",action="store_true")
-args=parser.parse_args()
+GRAPH_TITLE,args=common_argparse_alphas()
 
 x_linha=np.genfromtxt("../samples/entradas_x.txt")
 y_linha=np.genfromtxt("../samples/saidas_y.txt")
