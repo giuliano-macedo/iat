bb22319fa75c4f4c84d516799595897a266b4469
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-28 15:47:40 -0400

    better to read

--- ia/tpi/humoments.py
+++ ia/tpi/humoments.py
@@ -1,37 +1,42 @@
 import numpy as np
 #this code is adapted from https://github.com/llpinokio/proct2
-
-def regularMomentOrder(i,p,q):
-	ans=0
-	h,w=i.shape
-	for y in range(h):
-		for x in range(w):
-			if i[x][y]:
-				ans+=pow(x,p)*pow(y,q)
-	return ans
-def mu(i,a,xmean,ymean,p,q):
-	ans=0
-	h,w=i.shape
-	for y in range(h):
-		for x in range(w):
-			if i[x][y]:
-				ans+=pow((x-xmean),p)*pow((y-ymean),q)
-	return ans
-
 def humoments(i):
-	a=regularMomentOrder(i,1,1)
-	xm=regularMomentOrder(i,1,0)/a
-	ym=regularMomentOrder(i,0,1)/a
+	def M(p,q):
+		ans=0
+		h,w=i.shape
+		for y in range(h):
+			for x in range(w):
+				if i[x][y]:
+					ans+=pow(x,p)*pow(y,q)
+		return ans
+	def mu(p,q):
+		ans=0
+		h,w=i.shape
+		for y in range(h):
+			for x in range(w):
+				if i[x][y]:
+					ans+=pow((x-xbar),p)*pow((y-ybar),q)
+		return ans
+	# def mu(p,q):
+	# 	ans=0
+	# 	h,w=i.shape
+	# 	gamma=1+((p+q)/2)
+	# 	return eta(p,q)/pow(eta00,gamma)
+	m00=M(1,1)
+	xbar=M(1,0)/m00
+	ybar=M(0,1)/m00
 
-	u11=mu(i,a,xm,ym,1,1)
-	u20=mu(i,a,xm,ym,2,0)
-	u02=mu(i,a,xm,ym,0,2)
+	# eta00=eta(0,0)
 
-	u21=mu(i,a,xm,ym,2,1)
-	u12=mu(i,a,xm,ym,1,2)
+	u11=mu(1,1)
+	u20=mu(2,0)
+	u02=mu(0,2)
 
-	u30=mu(i,a,xm,ym,3,0)
-	u03=mu(i,a,xm,ym,0,3)
+	u21=mu(2,1)
+	u12=mu(1,2)
+
+	u30=mu(3,0)
+	u03=mu(0,3)
 
 	ans=[]
 
@@ -58,4 +63,6 @@ def humoments(i):
 
 	ans.append( (((3*u21)-u03)*(u30+u12)*(temp1-3*temp2)) - ((u30 - (3*u12))*(u21+u03)*((3*temp1) - temp2)))#phi7
 
+	# print(ans)
+	# breakpoint()
 	return ans
