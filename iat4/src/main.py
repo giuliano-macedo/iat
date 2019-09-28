4792b4aebde1f15d9ab29bc44d44df6657af3317
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-28 11:02:47 -0400

    train/eval

--- iat4/src/main.py
+++ iat4/src/main.py
@@ -19,7 +19,9 @@ def get_dataset_type(dataset_type):
 			x=args.method(img)
 			x.append(y)
 			ans.append(x)
-	return np.asarray(ans,dtype=np.float).T
+	ans=np.asarray(ans,dtype=np.float)
+	np.random.shuffle(ans)
+	return ans.T
 
 
 args=get_args()
@@ -31,46 +33,56 @@ if (args.dataset==os.path.join("..","dataset")) and( not os.path.isdir(args.data
 	download_and_extract_zip(url,args.dataset)
 print("preprocesing training")
 X_train=get_dataset_type("train")
+print("preprocesing evaluation")
+X_eval =get_dataset_type("evaluation")
+
+model_train=LogisticRegression(*X_train,alpha=args.alpha,normalize=True)
+model_eval=LogisticRegression(*X_eval,alpha=args.alpha,normalize=True) #TODO: preety hacky
 #---------------------------------------------------------------------------------
 #TODO MODULARIZE
-GRAPH_TITLE=""
-if args.interval.type=="range":
-	a,b,c=args.interval.ans;
-	GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para diferentes alphas que variam ${a:g}$ e ${b:g}$, passo {c:g}'
-elif args.interval.type=="list":
-	GRAPH_TITLE=rf'Gráfico de $J(\theta)$ para os alphas {args.interval.ans}'
+GRAPH_TITLE=rf"""Gráfico de $J(\theta)$ do dataset de treino e validação para o $\alpha={args.alpha}$ 
+ utilizando o(s) métodos(s) de {str(args.method)}"""
 
-ModelEntry=namedtuple("ModelEntry",["model","errs","accrs","j"])
 
-entries=[]
-for alpha in args.interval:
-	model=LogisticRegression(*X_train,alpha=alpha,normalize=True)
-	errs=np.zeros((args.no_it))
-	acrs=errs.copy()
-	for i in range(args.no_it):
-		acrs[i]=model.accuracy()
-		thetao,j=next(model)
-		errs[i]=j
-	entries.append(ModelEntry(model,errs,acrs,j))
-header=";".join(["alpha","j",*(f"t{i}" for i in range(len(entries[0].model.theta)))])
+j_train=np.zeros((args.no_it))
+acrs_train=j_train.copy()
+
+j_eval=j_train.copy()
+acrs_eval=j_train.copy()
+
+
+print("training alpha=",args.alpha)
+for i in tqdm(range(args.no_it)):
+	acrs_train[i]=model_train.accuracy()
+	j_train[i]=next(model_train)[1]
+	
+	model_eval.theta=model_train.theta.copy()
+	acrs_eval[i]=model_eval.accuracy()
+	j_eval[i]=next(model_eval)[1]
+
+header=";".join(["alpha","j",*(f"t{i}" for i in range(len(model_train.theta)))])
 np.savetxt("results.csv", [
-	(entry.model.alpha,entry.j,*entry.model.theta)
-	for entry in entries
+	(model_train.alpha,j_train[-1],*model_train.theta)
 ], delimiter=";",header=header)
 
 xs=np.array(range(1,args.no_it+1))
-for entry in entries:
-	plt.plot(xs,entry.errs,label=rf"$\alpha={entry.model.alpha}$")
+
+plt.plot(xs,j_train,label=rf"treino")
+plt.plot(xs,j_eval,label=rf"evaluação")
+
 plt.legend(fontsize=8)
 plt.xlabel('Números de iteração')
 plt.ylabel(r'$J(\theta)$')
 plt.title(GRAPH_TITLE)
 
 plt.figure()
-for entry in entries:
-	plt.plot(xs,entry.accrs,label=rf"$\alpha={entry.model.alpha}$")
+
+plt.plot(xs,acrs_train,label=rf"treino")
+plt.plot(xs,acrs_eval,label=rf"evaluação")
+
 plt.legend(fontsize=8)
 plt.xlabel('Números de iteração')
 plt.ylabel(r'acurácia')
 plt.title(GRAPH_TITLE.replace(r"$J(\theta)$","acurácia"))
+
 plt.show()
